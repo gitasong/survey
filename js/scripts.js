1d38d39585807cdd680dc2e71b6b642fe9dbce69
Kaden Shorf and Nicole Freed <nicole@>
2017-06-01 17:08:30 -0700

    fix bug: replace href with src in <scripts> link

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,16 @@ $("document").ready(function() {
     var nameInput = $("input#name").val();
     var foodInput = $("input#food").val();
     var musicInput = $("input#music").val();
-    //var eyesInput = $("input:radio[name=eyes]:checked").val();
-    //var birthInput = $("#birth").val();
-    //var colorInput = $("#color").val(); //need to convert color data to a color name or string?
+    var eyesInput = $("input:radio[name=eyes]:checked").val();
+    var birthInput = $("#birth").val();
+    var colorInput = $("#color").val(); //need to convert color data to a color name or string?
 
     $(".name").text(nameInput);
     $(".food").text(foodInput);
     $(".music").text(musicInput);
-    //$(".eyes").text(eyesInput);
-    //$(".birth").text(birthInput);
-    //$(".color").text(colorInput);
+    $(".eyes").text(eyesInput);
+    $(".birth").text(birthInput);
+    $(".color").text(colorInput);
 
     $("#result").show();
 
